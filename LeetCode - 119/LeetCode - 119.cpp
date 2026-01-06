c66626b5d178c597dfae74d83e541470e81caf05
ZiaynGZiyaNG <chen971023@gmail.com>
2026-01-07 00:11:42 +0800

    Create LeetCode - 119.cpp

---
+++ LeetCode - 119/LeetCode - 119.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) 
+    {
+        vector<vector<int>> dp(rowIndex + 1);
+
+        for (int i = 0; i <= rowIndex; i++)
+        {
+            dp[i].resize(i + 1, 1);
+            for (int j = 1; j < i; j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+
+        return dp[rowIndex];
+    }
+};
