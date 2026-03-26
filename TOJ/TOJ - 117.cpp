99074d55239a65fd7ca78a9bdc280a0c6904bd2f
ZiaynGZiyaNG <chen971023@gmail.com>
2026-03-26 21:19:29 +0800

    Create TOJ - 117.cpp

---
+++ TOJ/TOJ - 117.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n; cin >> n;
+	int mx = INT_MIN;
+	for (int i = 0; i < n; i++)
+	{
+		int a; cin >> a;
+		mx = max(a, mx);
+	}
+	cout << mx;
+}
