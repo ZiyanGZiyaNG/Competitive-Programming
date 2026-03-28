7ce2f6dc8a7b6db8acbf5a8bb514dc9a2bffdf55
ZiaynGZiyaNG <chen971023@gmail.com>
2026-03-29 00:52:39 +0800

    Create TOJ - 124.cpp

---
+++ TOJ/TOJ - 124.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a, b, c; cin >> a >> b >> c;
+	cout << 1997 + a << "/" << b << "/" << c << '\n';
+	string s1; cin >> s1;
+	cout << "Doodle Name: " << s1 << '\n';
+	string s2; cin >> s2;
+	cout << "Reviews: " << s2 << '\n';
+}
